a4df8053cc7745554d009b8aebb737a9aa1fa681
carlos <twincarlos98@gmail.com>
2024-06-06 14:49:55 -0400

    added logo link

--- src/app/(components)/(Navbar)/(components)/Logo.js
+++ src/app/(components)/(Navbar)/(components)/Logo.js
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import logo from "../../../(assets)/logo.png";
+import Link from "next/link";
 
 export function Logo() {
     return (
-        <Image className="logo" alt="logo"  src={logo} />
+        <Link href="/">
+            <Image className="logo" alt="logo"  src={logo} />
+        </Link>
     );
 };
