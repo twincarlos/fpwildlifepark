f17fb676033317fe0e8e300ec9ab91447a16282f
carlos <twincarlos98@gmail.com>
2024-06-06 04:28:35 -0400

    added a 5th home component

---
+++ src/app/(components)/(Home)/(components)/HomeComponent5/HomeComponent5.js
@@ -0,0 +1,19 @@
+export function HomeComponent5() {
+    return (
+        <section className="home-component home-component-5">
+            <div className="container">
+                <div className="email-icon">
+                    <i className="fa-solid fa-envelope" />
+                    <p>Get the newsletter</p>
+                </div>
+                <div className="sign-up-text">
+                    <h2>Sign up for wildlife news, Zoo activities, and more</h2>
+                </div>
+                <div className="sign-up-form">
+                    <input type="text" placeholder="Email Address" />
+                    <button>Submit</button>
+                </div>
+            </div>
+        </section>
+    );
+};
--- src/app/(components)/(Home)/Home.js
+++ src/app/(components)/(Home)/Home.js
@@ -3,6 +3,7 @@ import { HomeComponent1 } from "./(components)/HomeComponent1/HomeComponent1";
 import { HomeComponent2 } from "./(components)/HomeComponent2/HomeComponent2";
 import { HomeComponent3 } from "./(components)/HomeComponent3/HomeComponent3";
 import { HomeComponent4 } from "./(components)/HomeComponent4/HomeComponent4";
+import { HomeComponent5 } from "./(components)/HomeComponent5/HomeComponent5";
 
 export function Home() {
     return (
@@ -11,6 +12,7 @@ export function Home() {
             <HomeComponent2 />
             <HomeComponent3 />
             <HomeComponent4 />
+            <HomeComponent5 />
         </main>
     );
 };
