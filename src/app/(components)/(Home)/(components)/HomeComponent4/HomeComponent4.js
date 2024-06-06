013cb4ba100c07eb9ffb079f4df3a424618cfd1f
carlos <twincarlos98@gmail.com>
2024-06-06 04:05:11 -0400

    location section added

---
+++ src/app/(components)/(Home)/(components)/HomeComponent4/HomeComponent4.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import parkLocation from "../../../../assets/park-location.png";
+
+export function HomeComponent4() {
+    return (
+        <section className="home-component home-component-4">
+            <div className="container">
+                <div className="park-location">
+                    <Image alt="park-location" src={parkLocation} />
+                </div>
+                <div className="how-to-get-there">
+                    <i className="how-to-get-there-icon fa-solid fa-location-dot" />
+                    <p className="park-name">Fragile Planet Wildlife Park</p>
+                    <h2 className="getting-there">Getting Here</h2>
+                    <p className="park-address">34748 FM2480, Los Fresnos, TX 78566</p>
+                    <p className="location-description">Fragile Planet Wildlife Park is located just outside of Natureville, nestled in the beautiful green landscapes, minutes from Brownsville and Harlingen.</p>
+                    <p className="get-directions-button">Get Directions <i className="fa-solid fa-paper-plane" /></p>
+                </div>
+            </div>
+        </section>
+    );
+};
--- src/app/(components)/(Home)/Home.js
+++ src/app/(components)/(Home)/Home.js
@@ -2,6 +2,7 @@ import "./Home.css";
 import { HomeComponent1 } from "./(components)/HomeComponent1/HomeComponent1";
 import { HomeComponent2 } from "./(components)/HomeComponent2/HomeComponent2";
 import { HomeComponent3 } from "./(components)/HomeComponent3/HomeComponent3";
+import { HomeComponent4 } from "./(components)/HomeComponent4/HomeComponent4";
 
 export function Home() {
     return (
@@ -9,6 +10,7 @@ export function Home() {
             <HomeComponent1 />
             <HomeComponent2 />
             <HomeComponent3 />
+            <HomeComponent4 />
         </main>
     );
 };
