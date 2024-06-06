b21d7f3677527d0f6bd0befd5e8e23b1fa16d98c
carlos <twincarlos98@gmail.com>
2024-06-06 03:25:38 -0400

    added transition time for news and resources card

--- src/app/(components)/(Home)/(components)/HomeComponent3/HomeComponent3.js
+++ src/app/(components)/(Home)/(components)/HomeComponent3/HomeComponent3.js
@@ -1,8 +1,3 @@
-import cell2Image from "../../../../assets/cell-2.png";
-import cell3Image from "../../../../assets/cell-3.png";
-import cell4Image from "../../../../assets/cell-4.png";
-import Image from "next/image";
-
 export function HomeComponent3() {
     return (
         <section className="home-component home-component-3">
@@ -16,30 +11,36 @@ export function HomeComponent3() {
                             <p>Stay informed and engaged with the latest from Fragile Planet Wildlife Park. Our News & Resources section is your go-to source for comprehensive updates and valuable information.</p>
                         </div>
                     </div>
-                    <div className="container-cell">
+                    <div className="container-cell cell-2">
                         <div className="cell-description">
                             <p>Animals & Plants</p>
                         </div>
                         <div className="cell-image">
-                            <Image alt="cell-detail" src={cell2Image} />
+                            <p>
+                                Explore detailed profiles and fascinating facts about the diverse species of animals and plants at our park. Learn about their habitats, behaviors, and the conservation efforts dedicated to their protection.
+                            </p>
                         </div>
                     </div>
                 </div>
                 <div className="container-row">
-                    <div className="container-cell">
+                    <div className="container-cell cell-3">
                         <div className="cell-description">
                             <p>Education</p>
                         </div>
                         <div className="cell-image">
-                            <Image alt="cell-detail" src={cell3Image} />
+                            <p>
+                                Access a wealth of educational materials designed for all ages. From interactive lessons and activities to in-depth articles and videos, our resources aim to inspire and educate the next generation of wildlife advocates.
+                            </p>
                         </div>
                     </div>
-                    <div className="container-cell">
+                    <div className="container-cell cell-4">
                         <div className="cell-description">
                             <p>News & Events</p>
                         </div>
                         <div className="cell-image">
-                            <Image alt="cell-detail" src={cell4Image} />
+                            <p>
+                                Keep up with the latest news and upcoming events at Fragile Planet Wildlife Park. Discover exciting announcements, special programs, and opportunities to get involved in our conservation initiatives.
+                            </p>
                         </div>
                     </div>
                 </div>
