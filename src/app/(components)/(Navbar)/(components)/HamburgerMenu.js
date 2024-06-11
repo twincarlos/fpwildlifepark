f474e7aee25548771d52587365ef42fff84da597
carlos <twincarlos98@gmail.com>
2024-06-11 00:30:16 -0400

    made mobile responsive changes

--- src/app/(components)/(Navbar)/(components)/HamburgerMenu.js
+++ src/app/(components)/(Navbar)/(components)/HamburgerMenu.js
@@ -12,17 +12,17 @@ export function HamburgerMenu({ setOpenMenu }) {
                 </div>
                 <div className="hamburger-menu-item-container">
                     <div className="hamburger-menu-item">
-                        <Link href="/tickets">{/*<i className="fa-solid fa-ticket" />*/}Tickets</Link>
+                        <Link onClick={() => setOpenMenu(false)} href="/tickets">{/*<i className="fa-solid fa-ticket" />*/}Tickets</Link>
                     </div>
                 </div>
                 <div className="hamburger-menu-item-container">
                     <div className="hamburger-menu-item">
-                        <Link href="/tours">{/*<i className="fa-solid fa-tractor" />*/}Tours</Link>
+                        <Link onClick={() => setOpenMenu(false)} href="/tours">{/*<i className="fa-solid fa-tractor" />*/}Tours</Link>
                     </div>
                 </div>
                 <div className="hamburger-menu-item-container">
                     <div className="hamburger-menu-item">
-                        <Link href="/events">{/*<i className="fa-solid fa-calendar-days" />*/}Events</Link>
+                        <Link onClick={() => setOpenMenu(false)} href="/events">{/*<i className="fa-solid fa-calendar-days" />*/}Events</Link>
                     </div>
                 </div>
                 <div className="hamburger-menu-item-container title-only">
@@ -30,22 +30,22 @@ export function HamburgerMenu({ setOpenMenu }) {
                 </div>
                 <div className="hamburger-menu-item-container">
                     <div className="hamburger-menu-item">
-                        <Link href="/about/our-story">Our Story</Link>
+                        <Link onClick={() => setOpenMenu(false)} href="/about/our-story">Our Story</Link>
                     </div>
                 </div>
                 <div className="hamburger-menu-item-container">
                     <div className="hamburger-menu-item">
-                        <Link href="/about/our-team">Our Team</Link>
+                        <Link onClick={() => setOpenMenu(false)} href="/about/our-team">Our Team</Link>
                     </div>
                 </div>
                 <div className="hamburger-menu-item-container">
                     <div className="hamburger-menu-item">
-                        <Link href="/about/education">Education</Link>
+                        <Link onClick={() => setOpenMenu(false)} href="/about/education">Education</Link>
                     </div>
                 </div>
                 <div className="hamburger-menu-item-container">
                     <div className="hamburger-menu-item">
-                        <Link href="/about/animals-and-plants">Animals and Plants</Link>
+                        <Link onClick={() => setOpenMenu(false)} href="/about/animals-and-plants">Animals and Plants</Link>
                     </div>
                 </div>
                 <div className="hamburger-menu-item-container title-only">
@@ -53,22 +53,22 @@ export function HamburgerMenu({ setOpenMenu }) {
                 </div>
                 <div className="hamburger-menu-item-container">
                     <div className="hamburger-menu-item">
-                        <Link href="/contact-us/lets-connect">{"Let's Connect"}</Link>
+                        <Link onClick={() => setOpenMenu(false)} href="/contact-us/lets-connect">{"Let's Connect"}</Link>
                     </div>
                 </div>
                 <div className="hamburger-menu-item-container">
                     <div className="hamburger-menu-item">
-                        <Link href="/contact-us/volunteer-application">Volunteer Application</Link>
+                        <Link onClick={() => setOpenMenu(false)} href="/contact-us/volunteer-application">Volunteer Application</Link>
                     </div>
                 </div>
                 <div className="hamburger-menu-item-container">
                     <div className="hamburger-menu-item">
-                        <Link href="/contact-us/job-application">Job Application</Link>
+                        <Link onClick={() => setOpenMenu(false)} href="/contact-us/job-application">Job Application</Link>
                     </div>
                 </div>
                 <div className="hamburger-menu-item-container">
                     <div className="hamburger-menu-item">
-                        <Link href="/contact-us/internship-application">Internship Application</Link>
+                        <Link onClick={() => setOpenMenu(false)} href="/contact-us/internship-application">Internship Application</Link>
                     </div>
                 </div>
                 <div className="hamburger-menu-item-container title-only">
@@ -76,12 +76,12 @@ export function HamburgerMenu({ setOpenMenu }) {
                 </div>
                 <div className="hamburger-menu-item-container">
                     <div className="hamburger-menu-item">
-                        <Link href="/">English</Link>
+                        <Link onClick={() => setOpenMenu(false)} href="/">English</Link>
                     </div>
                 </div>
                 <div className="hamburger-menu-item-container">
                     <div className="hamburger-menu-item">
-                        <Link href="/">Spanish</Link>
+                        <Link onClick={() => setOpenMenu(false)} href="/">Spanish</Link>
                     </div>
                 </div>
             </div>
