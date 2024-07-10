5da55b2201753cea5636dd9fa24a00c9141d5275
carlos <twincarlos98@gmail.com>
2024-07-09 23:52:46 -0400

    updated look and feel of navbar

---
+++ src/app/(components)/(Navbar)/(components)/BigDropdownMenu.js
@@ -0,0 +1,38 @@
+import "../Navbar.css";
+import Link from "next/link";
+
+export function BigDropdownMenu({ dropdownHeaderIcon, dropdownHeaderValue, dropdownOptions }) {
+    return (
+        <div className="Large big-dropdow-menu">
+            <div className="big-dropdown-menu-header">
+                {dropdownHeaderIcon ? dropdownHeaderIcon : null}
+                <div className="big-dropdown-header-value">
+                    <p>{dropdownHeaderValue}</p>
+                </div>
+                <div className="big-dropdown-header-arrow">
+                    <i className="fa-solid fa-chevron-down" />
+                </div>
+            </div>
+            <div className={`big-dropdown-menu-body ${dropdownHeaderValue}`}>
+                {
+                    dropdownOptions.map((dropdownOption, index) => (
+                        <div key={index} className={`big-dropdown-option big-option-${index + 1}`}>
+                            <Link
+                                href={dropdownOption.path ? dropdownOption.path : "/"}>
+                                {dropdownOption.icon}
+                                <div className="big-dropdown-option-content">
+                                    <span className="big-dropdown-option-title">
+                                        {dropdownOption.value}
+                                    </span>
+                                    <span className="big-dropdown-option-description">
+                                        {dropdownOption.description}
+                                    </span>
+                                </div>
+                            </Link>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    );
+};
--- src/app/(components)/(Navbar)/(components)/Links.js
+++ src/app/(components)/(Navbar)/(components)/Links.js
@@ -3,7 +3,8 @@ import Link from "next/link";
 import { DropdownMenu } from "./DropdownMenu";
 import { useState } from "react";
 import { HamburgerMenu } from "./HamburgerMenu";
-import { GoogleTranslate } from "@/app/google-translate";
+import { BigDropdownMenu } from "./BigDropdownMenu";
+// import { GoogleTranslate } from "@/app/google-translate";
 
 export function Links() {
     const [openMenu, setOpenMenu] = useState(false);
@@ -13,22 +14,58 @@ export function Links() {
             <Link className="Large nav-link" href="/tickets"><i className="fa-solid fa-ticket" />Tickets</Link>
             <Link className="Large nav-link" href="/tours"><i className="fa-solid fa-tractor" />Tours</Link>
             <Link className="Large nav-link" href="/events"><i className="fa-solid fa-calendar-days" />Events</Link>
-            <DropdownMenu
+            {/* <DropdownMenu
                 dropdownHeaderIcon={<i className="fa-solid fa-paw" />}
                 dropdownHeaderValue="About"
                 dropdownOptions={[{ value: "Our Story", path: "/about/our-story" }, { value: "Our Team", path: "/about/our-team" }, { value: "Education", path: "/about/education" }, { value: "Animals & Plants", path: "/about/animals-and-plants" }]}
-            />
-            <DropdownMenu
+            /> */}
+            {/* <DropdownMenu
                 dropdownHeaderIcon={<i className="fa-solid fa-campground" />}
                 dropdownHeaderValue="Contact Us"
                 dropdownOptions={[{ value: "Let's Connect", path: "/contact-us/lets-connect" }, { value: "Volunteer Application", path: "/contact-us/volunteer-application" }, { value: "Job Application", path: "/contact-us/job-application" }, { value: "Internship Application", path: "/contact-us/internship-application" }]}
-            />
+            /> */}
             {/* <DropdownMenu
                 dropdownHeaderIcon={<i className="fa-solid fa-globe" />}
                 dropdownHeaderValue="English"
                 dropdownOptions={[{ value: "Español" }, { value: "English" }]}
             /> */}
             {/* <GoogleTranslate /> */}
+            <BigDropdownMenu
+                dropdownHeaderIcon={<i className="fa-solid fa-paw" />}
+                dropdownHeaderValue="About"
+                dropdownOptions={[
+                    {
+                        value: "Our Story", path: "/about/our-story", icon: <i className="fa-solid fa-hat-cowboy" />, description: "Discover the journey and mission behind our dedication to conservation and wildlife protection."
+                    },
+                    {
+                        value: "Our Team", path: "/about/our-team", icon: <i className="fa-solid fa-users" />, description: "Meet the passionate and diverse team of experts who bring our vision to life."
+                    },
+                    {
+                        value: "Education", path: "/about/education", icon: <i className="fa-solid fa-book-open-reader" />, description: "Explore our educational programs designed to inspire and inform all ages about wildlife conservation and environmental stewardship."
+                    },
+                    {
+                        value: "Animals & Plants", path: "/about/animals-and-plants", icon: <i className="fa-solid fa-cow" />, description: "Get to know the diverse species of animals and plants that call our park home."
+                    }
+                ]}
+            />
+            <BigDropdownMenu
+                dropdownHeaderIcon={<i className="fa-solid fa-campground" />}
+                dropdownHeaderValue="Contact Us"
+                dropdownOptions={[
+                    {
+                        value: "Let's Connect", path: "/contact-us/lets-connect", icon: <i className="fa-regular fa-comments" />, description: "Stay connected with us through social media, newsletters, and events. Join our community and support our cause."
+                    },
+                    {
+                        value: "Volunteer Application", path: "/contact-us/volunteer-application", icon: <i class="fa-brands fa-wpforms" />, description: "Become a part of our mission by volunteering with us. Fill out an application to join our team."
+                    },
+                    {
+                        value: "Job Application", path: "/contact-us/job-application", icon: <i className="fa-brands fa-wpforms" />, description: "Looking for a career in wildlife conservation? Check out our job openings and apply to work with us."
+                    },
+                    {
+                        value: "Internship Application", path: "/contact-us/internship-application", icon: <i className="fa-brands fa-wpforms" />, description: "Gain hands-on experience and learn from the experts by applying for an internship with us."
+                    }
+                ]}
+            />
             <i onClick={() => setOpenMenu(!openMenu)} className="menu-icon Small fa-solid fa-bars" />
             {
                 openMenu ? <HamburgerMenu setOpenMenu={setOpenMenu} /> : null
