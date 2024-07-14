4fd69205c0870e960281fa5e1b37b139e4dbe1e8
carlos <twincarlos98@gmail.com>
2024-07-13 22:46:51 -0400

    fixed local storage issue

--- src/app/context/LanguageContext.js
+++ src/app/context/LanguageContext.js
@@ -4,8 +4,8 @@ import { createContext, useState, useContext } from 'react';
 const LanguageContext = createContext();
 
 export const LanguageProvider = ({ children }) => {
-  const savedLanguage = localStorage.getItem("language");
-  const [language, setLanguage] = useState(savedLanguage || "English");
+  // const savedLanguage = localStorage.getItem("language");
+  const [language, setLanguage] = useState("English");
 
   return (
     <LanguageContext.Provider value={{ language, setLanguage }}>
