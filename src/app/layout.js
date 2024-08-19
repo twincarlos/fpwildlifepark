5ad26b9241d138d1c96d5382cfef5a1358965b8c
carlos rodriguez <88858893+twincarlos@users.noreply.github.com>
2024-08-19 18:24:27 -0400

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,6 @@ import { Footer } from "./(components)/(Footer)/Footer";
 import Script from "next/script";
 import dynamic from "next/dynamic";
 import { ModalProvider } from "./context/ModalContext";
-// import HiddenModal from "./(components)/HiddenModal/HiddenModal";
 const LanguageProvider = dynamic(() => import("./context/LanguageContext"), { ssr: false });
 
 const inter = Inter({ subsets: ["latin"] });
@@ -24,7 +23,6 @@ export default function RootLayout({ children }) {
         <LanguageProvider>
           <ModalProvider>
             <Navbar />
-            // <HiddenModal />
               {children}
             <Footer />
           </ModalProvider>
