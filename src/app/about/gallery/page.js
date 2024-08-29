8e79028bc1019f60e3cf638604956be6c0537174
carlos <twincarlos98@gmail.com>
2024-08-29 11:09:30 -0400

    hh

--- src/app/about/gallery/page.js
+++ src/app/about/gallery/page.js
@@ -1,19 +1,36 @@
 "use client"
 import "../About.css";
-import animal1 from "../../(assets)/animal-1.png";
-import animal2 from "../../(assets)/animal-2.png";
-import animal3 from "../../(assets)/animal-3.png";
-import animal4 from "../../(assets)/animal-4.png";
-import animal5 from "../../(assets)/animal-5.png";
-import animal6 from "../../(assets)/animal-6.png";
-import animal7 from "../../(assets)/animal-7.png";
-import animal8 from "../../(assets)/animal-8.png";
-import animal9 from "../../(assets)/animal-9.png";
-import animal10 from "../../(assets)/animal-10.png";
-import animal11 from "../../(assets)/animal-11.png";
+import pic1 from "@/app/(assets)/pic1.png";
+import pic2 from "@/app/(assets)/pic2.png";
+import pic3 from "@/app/(assets)/pic3.png";
+import pic4 from "@/app/(assets)/pic4.png";
+import pic5 from "@/app/(assets)/pic5.png";
+import pic6 from "@/app/(assets)/pic6.png";
+import pic7 from "@/app/(assets)/pic7.png";
+import pic8 from "@/app/(assets)/pic8.png";
+import pic9 from "@/app/(assets)/pic9.png";
+import pic10 from "@/app/(assets)/pic10.png";
+import pic11 from "@/app/(assets)/pic11.png";
+import pic12 from "@/app/(assets)/pic12.png";
+import pic13 from "@/app/(assets)/pic13.png";
+import pic14 from "@/app/(assets)/pic14.png";
+import pic15 from "@/app/(assets)/pic15.png";
+import pic16 from "@/app/(assets)/pic16.png";
+import pic17 from "@/app/(assets)/pic17.png";
+import pic18 from "@/app/(assets)/pic18.png";
+import pic19 from "@/app/(assets)/pic19.png";
+import pic20 from "@/app/(assets)/pic20.png";
+import pic21 from "@/app/(assets)/pic21.png";
+import pic22 from "@/app/(assets)/pic22.png";
+import pic23 from "@/app/(assets)/pic23.png";
+import pic24 from "@/app/(assets)/pic24.png";
+import pic25 from "@/app/(assets)/pic25.png";
+import pic26 from "@/app/(assets)/pic26.png";
+import pic27 from "@/app/(assets)/pic27.png";
+import pic28 from "@/app/(assets)/pic28.png";
+
 import Image from "next/image";
 import { useLanguage } from "@/app/context/LanguageContext";
-import Link from "next/link";
 
 export default function AnimalsAndPlants() {
     const {language} = useLanguage()
@@ -24,48 +41,90 @@ export default function AnimalsAndPlants() {
             </section>
             <section className="about-body">
                 <div className="about-content">
-                    <Link href="/about/gallery/american-bison" className="animal-container">
-                        <Image className="animal-img" alt="animal" src={animal2} />
-                        <div className="animal-info">
-                            <p className="animal-name">American Bison</p>
-                        </div>
-                    </Link>
-                    <Link href="/about/gallery/bactrian-camel" className="animal-container">
-                        <Image className="animal-img" alt="animal" src={animal3} />
-                        <div className="animal-info">
-                            <p className="animal-name">Bactrian Camel</p>
-                        </div>
-                    </Link>
-                    <Link href="/about/gallery/donkey" className="animal-container">
-                        <Image className="animal-img" alt="animal" src={animal5} />
-                        <div className="animal-info">
-                            <p className="animal-name">Donkey</p>
-                        </div>
-                    </Link>
-                    <Link href="/about/gallery/emu" className="animal-container">
-                        <Image className="animal-img" alt="animal" src={animal6} />
-                        <div className="animal-info">
-                            <p className="animal-name">Emu</p>
-                        </div>
-                    </Link>
-                    <Link href="/about/gallery/ostrich" className="animal-container">
-                        <Image className="animal-img" alt="animal" src={animal8} />
-                        <div className="animal-info">
-                            <p className="animal-name">Ostrich</p>
-                        </div>
-                    </Link>
                     <div className="animal-container">
-                        <Image className="animal-img" alt="animal" src={animal10} />
-                        <div className="animal-info">
-                            <p className="animal-name">Peahen</p>
-                        </div>
+                        <Image className="animal-img" alt="animal" src={pic1} />
                     </div>
-                    <Link href="/about/gallery/watusi" className="animal-container">
-                        <Image className="animal-img" alt="animal" src={animal11} />
-                        <div className="animal-info">
-                            <p className="animal-name">Watusi</p>
-                        </div>
-                    </Link>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
+                    <div className="animal-container">
+                        <Image className="animal-img" alt="animal" src={pic1} />
+                    </div>
                 </div>
             </section>
         </main>
